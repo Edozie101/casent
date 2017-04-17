7ce0201b0ece3d4824ddcbb4752098b5430a707b
edzye101 <edzye101@gmail.com>
2017-04-17 12:29:30 +0200

    added google meta tag

---
+++ test.java
@@ -0,0 +1,42 @@
+public class Test {
+  String name;
+
+  private int version;
+  private int numberOfIndividuals;
+
+  ArrayList<Integer> quizGrades = new ArrayList<Integer>();
+
+
+
+
+   Test (String name) {
+    this.name = name;
+    quizgrades.put
+  }
+  public  String getTest(){
+
+    return "It's a String!!!!" ;
+
+
+  }
+
+  public int looper(){
+    for(int i = 0; i < 10 ; i++) {
+      System.out.println(i);
+
+    }
+    return 0;
+  }
+  public static void main (String[] args){
+
+
+    Test helloworld = new Test("Edozie Izegbu");
+    System.out.println("Hello World" + ", my name is " + helloworld.name );
+    Test begin = new Test("Project X");
+    System.out.println(begin.getTest()) ;
+    begin.looper();
+
+
+  }
+
+}
